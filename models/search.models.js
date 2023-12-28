a60dc03919c7ec0bca037c9b59fd8615d12e78f6
Abbasblaze <abbas.raazaa40@gmail.com>
2023-12-28 15:42:20 +0500

    Microsoft To-Do APIS

--- models/search.models.js
+++ models/search.models.js
@@ -12,9 +12,13 @@ exports.search = (id, tasks) => {
   db.query(
     `SELECT id, tasks, reminders, plantasks, assignedtask
     FROM (
-        SELECT id, task AS tasks, NULL AS reminders, NULL AS plantasks, NULL AS assignedtask FROM task
+        SELECT id, task AS tasks, NULL AS reminders, NULL AS plantasks, NULL AS assignedtask FROM public.task
         UNION ALL
-        SELECT id, NULL AS tasks, reminder AS reminders, NULL AS plantasks, NULL AS assignedtask FROM reminder
+        SELECT id, NULL AS tasks, reminder AS reminders, NULL AS plantasks, NULL AS assignedtask FROM public.reminder
+        UNION ALL
+        SELECT id, NULL AS tasks, NULL AS reminders, plantasks, NULL AS assignedtask FROM public.plannedtasks
+        UNION ALL
+        SELECT id, NULL AS tasks, NULL AS reminders, NULL AS plantasks, assignedtask FROM public.assigned
     ) AS combined
     WHERE tasks IS NOT NULL OR reminders IS NOT NULL OR plantasks IS NOT NULL OR assignedtask IS NOT NULL;
     `,
